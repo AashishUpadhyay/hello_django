3ee4adf1b5c622807e3ad41ba7c5480302edccdf
Aashish Upadhyay <aashishupadhyay@Aashishs-MacBook-Pro.local>
2021-02-24 23:07:17 -0500

    formatting changed

--- hello/urls.py
+++ hello/urls.py
@@ -6,9 +6,8 @@ home_list_view = views.HomeListView.as_view(
     # :5 limits the results to the five most recent
     queryset=LogMessage.objects.order_by("-log_date")[:5],
     context_object_name="message_list",
-    template_name="hello/home.html",
-)
-̀
+    template_name="hello/home.html")
+
 urlpatterns = [
     path("", home_list_view, name="home"),
     path("hello/<name>", views.hello_there, name="hello_there"),
